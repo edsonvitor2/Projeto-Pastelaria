e559f7fd6a7ab05ca3c9cf19971dd95a6fafef0b
Edson Vitor <edson_vitor10@hotmail.com>
2023-07-29 16:16:46 -0300

    atualização balcão, informações caixa

--- jsOperacao/Controller/controllerCaixa.js
+++ jsOperacao/Controller/controllerCaixa.js
@@ -1,5 +1,6 @@
 class Caixa{
     constructor(){
+        this.idcaixa;
 
         this.initButtons();
         this.listarCaixa();
@@ -14,7 +15,6 @@ class Caixa{
     }
 
     listarPedidos(caixa){
-        console.log(caixa);
         firebase.database().ref("pedidos").on("value",element=>{
 
             let table = document.querySelector("#pedido");
@@ -26,7 +26,6 @@ class Caixa{
                 e.forEach(a=>{
                 let dados = a.val();
 
-                console.log(dados.caixa); 
                     if(dados.caixa == caixa){
                         let cliente = dados.cliente;
                         let formaPagamento = dados.FormaPagamento;
@@ -68,6 +67,13 @@ class Caixa{
         });
     }
 
+    receberValores(id){
+        console.log(id);
+
+        
+        //document.querySelector("").value = 'a';
+    }
+
     listarCaixa(){
 
         let table = document.querySelector(".tabelaCaixa");
@@ -91,19 +97,25 @@ class Caixa{
             table.appendChild(tr);
 
             tr.querySelector("#abrir").addEventListener("click",e=>{
-                firebase.database().ref('Caixas').child(key).on('value',e =>{
+                
+                firebase.database().ref('Caixas').child(key).once('value',e =>{
                     let dados = e.val();
                     document.querySelector("#data2").value = dados.data;
                     document.querySelector("#horainicio").value = dados.horaInicio;
                     document.querySelector("#horafim").value = dados.horaFim;
                     let idcaixa = tr.querySelector(".id").innerText;
                     this.listarPedidos(idcaixa);
-                })
+                    this.idcaixa = idcaixa;
+                    this.receberValores(key);
+                });
+                
             });
 
             });
         });
     }
+
+
 }
 
 var caixa = new Caixa();
