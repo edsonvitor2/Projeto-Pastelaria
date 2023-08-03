77239a35629178a587326444165b2b477dab7ff5
Edson Vitor <edson_vitor10@hotmail.com>
2023-08-03 10:55:08 -0300

    atualização Caixas
    abertura, fechamento, e visualização de pedidos.

--- jsOperacao/Controller/controllerCaixa.js
+++ jsOperacao/Controller/controllerCaixa.js
@@ -11,6 +11,17 @@ initButtons(){
 
     let fecharCaixa = document.querySelector("#fechar-caixa");
 
+    let fecharCaixaVisu = document.querySelector(".fechar-caixa");
+
+    let fecharDesc = document.querySelector(".fecharDesc");
+
+
+    fecharDesc.addEventListener('click',e=>{
+        document.querySelector(".descricao").style.display ='none';
+    })
+    fecharCaixaVisu.addEventListener("click",e=>{
+        document.querySelector(".caixa").style.display = 'none';
+    });
     abrirCaixa.addEventListener("click",e=>{
         this.abrirCaixa();
     });
@@ -41,6 +52,7 @@ fecharCaixa(){
         status,
         horaFim
     });
+    alert("Caixa fechado Com Sucesso!!!");
 }
 
 diferenca(){
@@ -50,43 +62,43 @@ diferenca(){
     let inputPix = document.querySelector("#caixa-pix");
 
     inputPix.addEventListener("keyup",e=>{
-        let pixSis = document.querySelector("#sis-pix").value;
+        let pixsis = document.querySelector("#sis-pix").value;
 
         let a = event.target.value;
         var pix = parseFloat(a);
-        let diferenca = pix - pixSis;
+        let diferenca = pix - pixsis;
 
         document.querySelector("#dif-pix").value = diferenca.toFixed(2);
         this.calcularDiferenca();
     });
 
     inputDinheiro.addEventListener("keyup",e=>{
-        let dinheiroSis = document.querySelector("#sis-dinheiro").value;
+        let dinheirosis = document.querySelector("#sis-dinheiro").value;
 
         let a = event.target.value;
         var dinheiro = parseFloat(a);
-        let diferenca = dinheiro - dinheiroSis;
+        let diferenca = dinheiro - dinheirosis;
 
         document.querySelector("#dif-dinheiro").value = diferenca.toFixed(2);
         this.calcularDiferenca();
     });
 
     inputDebito.addEventListener("keyup",e=>{
-        let debitoSis = document.querySelector("#sis-debito").value;
+        let debitosis = document.querySelector("#sis-debito").value;
 
         let a = event.target.value;
         var debito = parseFloat(a);
-        let diferenca = debito - debitoSis;
+        let diferenca = debito - debitosis;
         document.querySelector("#dif-debito").value = diferenca.toFixed(2);
         this.calcularDiferenca();
     });
 
     inputCredeito.addEventListener("keyup",e=>{
-        let creditoSis = document.querySelector("#sis-credito").value;
+        let creditosis = document.querySelector("#sis-credito").value;
 
         let a = event.target.value;
         var credito = parseFloat(a);
-        let diferenca = credito - creditoSis;
+        let diferenca = credito - creditosis;
         document.querySelector("#dif-credito").value = diferenca.toFixed(2);
         
         this.calcularDiferenca();
@@ -102,9 +114,9 @@ calcularDiferenca() {
     let somaTotal = debito + credito + dinheiro + pix;
     document.querySelector("#caixa-total").value = somaTotal.toFixed(2);
 
-    let totalSis = document.querySelector("#sis-total").value;
+    let totalsis = document.querySelector("#sis-total").value;
 
-    let difTotal = totalSis - somaTotal;
+    let difTotal = totalsis - somaTotal;
 
     document.querySelector("#dif-total").value = difTotal;
   }
@@ -112,17 +124,47 @@ calcularDiferenca() {
 listarValoresCaixaFechado(){
 
     if(this.status  == 'aberto'){
-        console.log("aberto");
+        document.querySelector("#dif-pix").value = '0,00';
+        document.querySelector("#dif-debito").value = '0,00';
+        document.querySelector("#dif-credito").value = '0,00';
+        document.querySelector("#dif-dinheiro").value = '0,00';
+        document.querySelector("#dif-total").value = '0,00';
+
+    document.querySelector("#caixa-pix").value = '0,00';
+
+    document.querySelector("#caixa-dinheiro").value = '0,00';
+
+    document.querySelector("#caixa-credito").value ='0,00';
+
+    document.querySelector("#caixa-debito").value = '0,00';
+
+    document.querySelector("#caixa-total").value = '0,00';
     }else{
         firebase.database().ref('Caixas').child(this.keycaixa).once('value',e =>{
            
         let data = e.val();
+
         let debito = data.debito;
         let credito =data.credito;
         let pix = data.pix;
         let dinheiro = data.dinheiro;
         let total = pix + debito + credito + dinheiro;
-    
+        let sisdebito = document.querySelector("#sis-debito").value;
+        let siscredito = document.querySelector("#sis-credito").value;
+        let sispix = document.querySelector("#sis-pix").value;
+        let sisdinheiro = document.querySelector("#sis-dinheiro").value;
+        let difdebito = debito - sisdebito;
+        let difcredito = credito - siscredito;
+        let difpix = pix - sispix;
+        let difdinheiro = dinheiro - sisdinheiro;
+        let diftotal = difdebito + difcredito + difpix + difdinheiro;
+
+        document.querySelector("#dif-pix").value = difpix.toFixed(2);
+        document.querySelector("#dif-debito").value = difdebito.toFixed(2);
+        document.querySelector("#dif-credito").value = difcredito.toFixed(2);
+        document.querySelector("#dif-dinheiro").value = difdinheiro.toFixed(2);
+        document.querySelector("#dif-total").value = diftotal.toFixed(2);
+
     document.querySelector("#caixa-pix").value = pix.toFixed(2);
 
     document.querySelector("#caixa-dinheiro").value = dinheiro.toFixed(2);
@@ -130,8 +172,6 @@ listarValoresCaixaFechado(){
     document.querySelector("#caixa-credito").value = credito.toFixed(2);
 
     document.querySelector("#caixa-debito").value = debito.toFixed(2);
-
-    
 
     document.querySelector("#caixa-total").value = total.toFixed(2);
 
@@ -159,6 +199,7 @@ listarPedidos(caixa) {
 
             e.forEach(a => {
                 let dados = a.val();
+                let key = a.key;
 
                 if (dados.caixa == caixa) {
                     let cliente = dados.cliente;
@@ -204,33 +245,101 @@ listarPedidos(caixa) {
                         <td>${valor.toFixed(2)}</td>
                         <td>${formaPagamento}</td>
                         <td>
+                            <button id="btn-descrição">
                             Descrição
+                            </button>
                         </td>
                     `;
                     table.appendChild(tr);
+                    tr.querySelector("#btn-descrição").addEventListener("click",e=>{
+
+                        document.querySelector(".descricao").style.display ='block';
+
+                        let descPedido = document.querySelector(".pedidosDesc");
+                        
+                        descPedido.innerHTML = '';
+
+                        firebase.database().ref("pedidos").child(pedido).child(key).once('value',snapshot=>{
+                            let val = snapshot.val();
+                            let item = val.itens;
+                            item.forEach(e=>{
+                                let itens = e;
+
+                                let trPedidos = document.createElement('tr');
+
+                        trPedidos.innerHTML = ` 
+                        <td>${itens.produto}</td>
+                        <td>${itens.sabor}</td>
+                        <td>${itens.quantidade}</td>
+                        <td>${itens.valor}</td>
+                    `;
+                    descPedido.appendChild(trPedidos);
+                            })
+                        })
+                    })
                 }
             });
         });
     });
 }
 
-    abrirCaixa(){
-        
-        let data = document.querySelector("#data").value;
-        let turno = document.querySelector("#turno").value;
-        let fundoCaixa = document.querySelector("#fundo").value;
-        var id = 0;
-        let status = "aberto"; 
-        let dataAtual = new Date();
-        let hora = dataAtual.getHours();
-        let minutos = dataAtual.getMinutes();
-        let horaInicio = `${hora}:${minutos}`;
-        let horaFim = "00:00";
-        
-        firebase.database().ref("Caixas").push({
-            id,data,turno,status,fundoCaixa,horaInicio,horaFim
-        });
-    }
+abrirCaixa() {
+    // Obter os valores do novo caixa
+    let data = document.querySelector("#data").value;
+    let turno = document.querySelector("#turno").value;
+    let fundoCaixa = document.querySelector("#fundo").value;
+    let status = "aberto"; 
+    let dataAtual = new Date();
+    let hora = dataAtual.getHours();
+    let minutos = dataAtual.getMinutes();
+    let horaInicio = `${hora}:${minutos}`;
+    let horaFim = "00:00";
+
+    // Primeiro, obtemos a referência para a lista de Caixas no Firebase e ordenamos pela chave ID em ordem decrescente.
+    const caixasRef = firebase.database().ref("Caixas").orderByChild("id").limitToLast(1);
+
+    // Em seguida, usamos a função once() para obter o último caixa registrado.
+    caixasRef.once("value", snapshot => {
+        // Verificamos se há algum caixa registrado.
+        if (snapshot.exists()) {
+            // Se existir, pegamos o valor do ID do último caixa.
+            const ultimoCaixa = Object.values(snapshot.val())[0];
+            const ultimoCaixaId = ultimoCaixa.id;
+
+            // Verificamos se o último caixa está fechado.
+            if (ultimoCaixa.status === "fechado") {
+                // Se estiver fechado, somamos 1 ao ID para obter o ID do próximo caixa.
+                const novoId = ultimoCaixaId + 1;
+
+                // Finalmente, criamos o novo caixa no Firebase com o novo ID.
+                firebase.database().ref("Caixas").push({
+                    id: novoId,
+                    data,
+                    turno,
+                    status,
+                    fundoCaixa,
+                    horaInicio,
+                    horaFim
+                });
+                alert("Caixa Aberto Com Sucesso!!!");
+            } else {
+                // Caso o último caixa esteja aberto, exibimos uma mensagem de erro.
+                alert("Aviso: Há um caixa aberto. Por favor, feche o caixa atual antes de abrir um novo.");
+            }
+        } else {
+            // Caso não haja nenhum caixa registrado ainda (novo estabelecimento), criamos o primeiro caixa com ID 1.
+            firebase.database().ref("Caixas").push({
+                id: 1,
+                data,
+                turno,
+                status,
+                fundoCaixa,
+                horaInicio,
+                horaFim
+            });
+        }
+    });
+}
 
     listarCaixa(){
 
@@ -255,17 +364,25 @@ listarPedidos(caixa) {
             table.appendChild(tr);
 
             tr.querySelector("#abrir").addEventListener("click",e=>{
-              
+                document.querySelector(".caixa").style.display = 'block';
                 firebase.database().ref('Caixas').child(key).once('value',e =>{
                     this.keycaixa = key;
                    
-                    console.log(this.keycaixa);
                     let dados = e.val();
                     this.status = dados.status;
+
                     document.querySelector("#data2").value = dados.data;
+
                     document.querySelector("#horainicio").value = dados.horaInicio;
+
                     document.querySelector("#horafim").value = dados.horaFim;
+
+                    document.querySelector("#status").value = dados.status;
+
+                    document.querySelector("#fundoCaixa").value = dados.fundoCaixa;
+
                     let idcaixa = tr.querySelector(".id").innerText;
+
                     this.listarPedidos(idcaixa);
                     this.listarValoresCaixaFechado();
                 });
