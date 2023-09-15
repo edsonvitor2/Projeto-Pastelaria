1116f37c07e0e7997fe77ebbac59a4dff9df3c9b
Edson Vitor de Sousa <edson_vitor10@hotmail.com>
2023-09-15 01:39:06 -0300

    troca de banco de dados
    adicionando banco de dados para deixar a mostra e qualquer pessoa poder testar

--- jsOperacao/FirebaseInit.js
+++ jsOperacao/FirebaseInit.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAr475NuMK-19-UWvL8DxosgXYjrnjKe48",
-  authDomain: "vila-teste-21fb8.firebaseapp.com",
-  databaseURL: "https://vila-teste-21fb8-default-rtdb.firebaseio.com",
-  projectId: "vila-teste-21fb8",
-  storageBucket: "vila-teste-21fb8.appspot.com",
-  messagingSenderId: "424731257421",
-  appId: "1:424731257421:web:2e33b565e70ea275b58a04",
-  measurementId: "G-EJS33T24YR"
+  apiKey: "AIzaSyBsPZBkaR-lGZlb25Bt8bkaM1AwML7ZdcY",
+  authDomain: "pastel-do-chaves.firebaseapp.com",
+  databaseURL: "https://pastel-do-chaves-default-rtdb.firebaseio.com",
+  projectId: "pastel-do-chaves",
+  storageBucket: "pastel-do-chaves.appspot.com",
+  messagingSenderId: "480801230089",
+  appId: "1:480801230089:web:7e2c192549737c420317d8",
+  measurementId: "G-J0LJ22GSFE"
 };
     
   firebase.initializeApp(firebaseConfig);
